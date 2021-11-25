9136cd3c19e20607537593c5e352c03a21ac783c
Bizso96 <bizso96@gmail.com>
2021-11-25 02:24:35 +0200

    Finished data processing

---
+++ main.py
@@ -0,0 +1,9 @@
+from minc2500 import Minc2500
+from config import *
+
+minc2500 = Minc2500()
+
+minc2500.data_location = MINC2500_PATH
+minc2500.read_data()
+
+print()
---
+++ minc2500.py
@@ -0,0 +1,73 @@
+import cv2
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from config import *
+
+
+class Minc2500:
+    def __init__(self):
+        self.__data_location = None
+        self.__materials = [
+            "fabric",
+            "foliage",
+            "glass",
+            "leather",
+            "metal",
+            "paper",
+            "plastic",
+            "stone",
+            "water",
+            "wood"
+        ]
+
+        self.__train_data = {}
+        self.__test_data = {}
+
+    @property
+    def data_location(self):
+        return self.__data_location
+
+    @data_location.setter
+    def data_location(self, value):
+        self.__data_location = value
+
+    def read_data(self):
+        if self.__data_location is None:
+            raise Exception("Data location not set")
+
+        i = 0
+
+        for material in self.__materials:
+            path = f'{self.__data_location}/images/{material}'
+
+            print("Reading in: " + path)
+
+            images = []
+            i = 0
+
+            for filename in os.listdir(path):
+                if i >= IMAGES_PER_CATEGORY:
+                    i = 0
+                    break
+
+                i += 1
+
+                if filename.endswith(".jpg"):
+                    print(" " + filename)
+                    img = cv2.imread(path + "/" + filename)
+                    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                    img = cv2.resize(img, (224, 224))
+
+                    images.append(img)
+
+            images = np.array(images)
+            np.random.shuffle(images)
+
+            split_index = int(images.shape[0] * TRAIN_TEST_RATIO)
+
+            self.__train_data[material], self.__test_data[material] = np.split(images, [split_index])
+
+            for img in self.__train_data[material]:
+                plt.imshow(img)
+                plt.show()
